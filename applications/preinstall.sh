7ce3506160a9d068b5005276491c50c18fe50a60
ka1em <wangkaiminchn@gmail.com>
2025-07-31 11:35:33 +0800

    update

--- applications/preinstall.sh
+++ applications/preinstall.sh
@@ -4,7 +4,9 @@ first_run=$(getprop persist.tesla_android.first_run)
 echo ${first_run}
 if [ "${first_run}" != "false" ]; then
 
-   pm install -g "/vendor/etc/preinstall/AutoKit.apk_"
+   #pm install -g "/vendor/etc/preinstall/AutoKit.apk_"
+   #pm install -g "/vendor/etc/preinstall/gps-connector.apk_"
+   #pm install -g "/vendor/etc/preinstall/gps-connector-service.apk_"
 
    setprop persist.tesla_android.first_run false
 fi
